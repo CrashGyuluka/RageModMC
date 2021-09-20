67edace60427cade12ef6f7ace6860cdf8bdaae7
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2021-09-20 20:36:00 +0200

    teszt6

---
+++ src/main/java/net/mcreator/ragemod/item/Tezst5Item.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.ragemod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.ragemod.RagemodModElements;
+
+@RagemodModElements.ModElement.Tag
+public class Tezst5Item extends RagemodModElements.ModElement {
+	@ObjectHolder("ragemod:tezst_5")
+	public static final Item block = null;
+	public Tezst5Item(RagemodModElements instance) {
+		super(instance, 1238);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("tezst_5");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
