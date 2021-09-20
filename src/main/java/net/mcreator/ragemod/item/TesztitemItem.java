a1dc8a2c84edba6d1e3f3be95fa9c053fa33a564
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2021-09-20 20:09:56 +0200

    teszt2

---
+++ src/main/java/net/mcreator/ragemod/item/TesztitemItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.ragemod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.ragemod.RagemodModElements;
+
+@RagemodModElements.ModElement.Tag
+public class TesztitemItem extends RagemodModElements.ModElement {
+	@ObjectHolder("ragemod:tesztitem")
+	public static final Item block = null;
+	public TesztitemItem(RagemodModElements instance) {
+		super(instance, 1237);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("tesztitem");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
