78a4725f427d892613133f012adf56ba9cce5340
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2021-11-02 20:49:29 +0100

    21.11.02-20.49

---
+++ src/main/java/net/mcreator/ragemod/block/AlienCrystalBlockBlock.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.ragemod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.ragemod.itemgroup.TermeszettabItemGroup;
+import net.mcreator.ragemod.RagemodModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@RagemodModElements.ModElement.Tag
+public class AlienCrystalBlockBlock extends RagemodModElements.ModElement {
+	@ObjectHolder("ragemod:alien_crystal_block")
+	public static final Block block = null;
+	public AlienCrystalBlockBlock(RagemodModElements instance) {
+		super(instance, 1443);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(TermeszettabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.PLANTS).sound(SoundType.BASALT).hardnessAndResistance(1.5f, 10f).setLightLevel(s -> 3)
+					.harvestLevel(2).harvestTool(ToolType.PICKAXE).setRequiresTool().speedFactor(1.1f).jumpFactor(1.1f)
+					.setNeedsPostProcessing((bs, br, bp) -> true).setEmmisiveRendering((bs, br, bp) -> true));
+			setRegistryName("alien_crystal_block");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
